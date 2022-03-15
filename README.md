2c6fdf2d426701dac9c4bb480309ba3a35a27658
hkjang <gagagiga@naver.com>
2022-03-15 13:53:55 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+node-red-contrib-pdfparse
+================
+
+Node-RED node for pdf-parse
+
+
+
+## Install
+
+To install the stable version use the `Menu - Manage palette - Install`
+option and search for node-red-contrib-pdfparse, or run the following
+command in your Node-RED user directory, typically `~/.node-red`
+
+    npm install node-red-contrib-pdfparse
+
+## wrapper pdfparse
+Node.js module to scrape application data from the Google Play store
+- https://www.npmjs.com/package/pdf-parse
+
+
---
+++ node.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const pdf = require('pdf-parse');
+module.exports = function (RED) {
+    function FunctionNode(n) {
+        RED.nodes.createNode(this, n);
+
+        var node = this;
+        this.name = n.name;
+        for (var key in n) {
+            node[key] = n[key] || "";
+        }
+
+        this.on('input', function (msg) {
+            for (var i in msg) {
+                if (i !== 'req' | i !== 'res' | i !== 'payload' | i !== 'send' | i !== '_msgid') {
+                    node[i] = node[i] || msg[i];
+                }
+            }
+            try{
+                let dataBuffer = fs.readFileSync(node.path);
+
+                pdf(dataBuffer).then(function(data) {
+                    msg.payload = data;
+                    node.send(msg);
+                });
+
+            }catch (e){
+                node.error(e);
+            }
+        });
+    }
+    RED.nodes.registerType("pdfparse", FunctionNode, {});
+};
+
